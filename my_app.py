0085612fea19e59a24df1b6ad7945d2fe7f0a233
Ubuntu <ubuntu@ip-172-31-41-82.eu-west-3.compute.internal>
2023-05-07 14:43:23 +0000

    Add last update of the app

--- my_app.py
+++ my_app.py
@@ -1,32 +1,37 @@
 from dash import Dash, dcc, html, Output, Input, State, callback
+import plotly.express as px
 import random
 
 
-# Définissez la question et la réponse correcte
-question = "Quelle commande permet de créer une nouvelle branche ?"
-correct_answer = "git branch"
 
 app = Dash(__name__)
+server=app.server
+
+
+question = ["Quelle commande permet de créer une nouvelle branche ?", "Quel est l'opérateur de comparaison qui permet de vérifier si deux valeurs sont égales ?"]
+correct_answer = ["git branch", "=="]
+
+indexQuestion=random.randint(0, len(question)-1)
+choixQuestion=question[indexQuestion]
 
 app.layout = html.Div([
     html.H1("Quiz"),
-    html.P(question),
+    html.P("Question : " + choixQuestion),
     dcc.Input(id='user-input', type='text', value='', placeholder='Entrez votre réponse ici...'),
     html.Button('Vérifier la réponse', id='submit-button', n_clicks=0),
     html.Div(id='output')
 ])
 
 
-# Créez une fonction de rappel pour vérifier la réponse
 @app.callback(
-    Output('output', 'children'),
-    [Input('submit-button', 'n_clicks')],
-    [State('user-input', 'value')]
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='submit-button', component_property='n_clicks')],
+    [State(component_id='user-input', component_property='value')]
 )
 
 def check_answer(n_clicks, user_answer):
     if n_clicks > 0:
-        if user_answer.lower() == correct_answer.lower():
+        if user_answer.lower() == correct_answer[indexQuestion].lower():
             return html.P("Bonne réponse !")
         else:
             return html.P(f"Mauvaise réponse. La réponse était {correct_answer}")
