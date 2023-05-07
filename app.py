f418b1398cc36ab3b2642c85ba426cfbde2acbe4
Ubuntu <ubuntu@ip-172-31-41-82.eu-west-3.compute.internal>
2023-05-07 16:13:33 +0000

    Add last version of the app

--- app.py
+++ app.py
@@ -1,70 +1,72 @@
-from dash import Dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
+from dash import Dash, dcc, html, Output, Input, State, callback
 import random
 
-# Liste des questions et réponses possibles
-questions = {
-    "Git": "Quelle commande permet de créer une nouvelle branche ?",
-    "Python": "Quel est l'opérateur de comparaison qui permet de vérifier si deux valeurs sont égales ?",
-    "Linux": "Quelle commande permet d'afficher le contenu d'un fichier ?"
-}
-
-
-reponses = {
-    "Git": "git branch",
-    "Python": "==",
-    "Linux": "cat"
-}
-
-# Fonction pour choisir une question aléatoire
-def choisir_question():
-    return random.choice(questions)
-
-# Création de l'application Dash
 app = Dash(__name__)
+server=app.server
 
-# Layout de la page
-app.layout = html.Div([
-    html.H1("Questionnaire"),
-    html.H2(id="question"),
-    dcc.Input(id="reponse-input", type="text", value=""),
-    html.Button("Vérifier la réponse", n_clicks=0, id="submit-button"),
-    html.H3(id="reponse")
-])
+question = ["Quelle commande permet de créer une nouvelle branche ?", 
+            "Quel est l'opérateur de comparaison qui permet de vérifier si deux valeurs sont égales ?", 
+            "Quelle commande linux permet de lister le contenu d'un répertoire ?", 
+            "Quelle commande permet de changer de répertoire ?", 
+            "Quelle commande permet de supprimer un répertoire et son contenu ?",
+            "Quelle commande permet de rechercher une chaine de caractère dans un fichier ?", 
+            "Quelle commande permet de changer de branche ?", 
+            "Quelle commande permet de créer une copie locale d'une reponsitory git ?", 
+            "Quelle commande linux permet de planifier l'exécution de commandes ou de scripts à intervalles réguliers  ?", 
+            "Quelle package python permet la création d'un tableau de bord ?", 
+            "Quelle commande permet d'afficher le contenu d'un fichier ?", 
+            "Quel outils permet de transférer des données en ligne vers un terminal ?", 
+            'cat fichier.txt | grep -oP "linux\s\w+" : renvoie le mot linux seul. Vrai ou Faux ?', 
+            "Qu'est ce que Linux ? (2 mots)", 
+            "Quelle est l'extention pour un script shell ? (attention au '.')", 
+            "La commande : */5 * * * * /chemin/vers/le/script.sh, exécute le script.sh toutes les 5 minutes. Vrai ou Faux ?", 
+            "Quelle est la commande pour créer un nouveau repository en linux ?", 
+            "Quel est le symbole d'un chevron en linux ?", 
+            "Quelle est la commande pour saisir du texte dans un fichier texte linux ? (4 lettres)", 
+            "Quelle est la commande pour changer les permissions d'un fichier ?", 
+            "Quelle sont les lettres permettant à un utilisateur de lire, écrire et exécuter un fichier ?", 
+            "Quelle commande permet d'exécuter une command en tant que super utilisateur ?"]
 
-# Fonction pour choisir une question aléatoire
-def choisir_question():
-    categorie = random.choice(list(questions.keys()))
-    question = questions[categorie]
-    reponse = reponses[categorie]
-    return question, reponse
+correct_answer = ["git branch", "==", "ls", "cd", "rm -rv",
+                  "grep", "git checkout", "git clone", "cron", "dash",
+                  "cat", "curl", "Faux", "Système d'exploitation", ".sh", 
+                  "Vrai", "mkdir", ">", "echo", "chmod", 
+                  "rwx", "sudo"]
+
+def serve_layout():
+    indexQuestion=random.randint(0, len(question)-1)
+    choixQuestion=question[indexQuestion]
 
 
-# Callback pour afficher une question aléatoire
+    layout = html.Div([
+        html.H1("Rattrapage Git, Python, Linux"),
+        html.H2("Question : " + choixQuestion, id="question"),
+        dcc.Input(id='user-input', type='text', value='', placeholder='Entrez votre réponse ici...'),
+        html.Button('Vérifier la réponse', id='submit-button', n_clicks=0),
+        html.Div(id='output')
+    ])
+    return layout
+
+app.layout=serve_layout
+app.title = "Quiz RG"
+
+
 @app.callback(
-    Output("question", "children"),
-    Input("submit-button", "n_clicks"))
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='submit-button', component_property='n_clicks')],
+    [State(component_id='user-input', component_property='value'),
+     State(component_id='question', component_property='children')]
+)
 
-def afficher_question(n_clicks):
-    question = choisir_question()
-    return question[0]
-
-# Callback pour vérifier la réponse
-@app.callback(
-    Output("reponse", "children"),
-    [Input("submit-button", "n_clicks")],
-    [State("reponse-input", "value")])
-
-
-def verifier_reponse(n_clicks, reponse_utilisateur):
-    reponse_attendue = choisir_question()[1]
-    if reponse_utilisateur == reponse_attendue:
-        return "Bonne réponse !"
+def check_answer(n_clicks, user_answer, question_text):
+    if n_clicks > 0:
+        indexQuestion = question.index(question_text[len("Question : "):])
+        if user_answer.lower() == correct_answer[indexQuestion].lower():
+            return html.P("Bonne réponse !")
+        else:
+            return html.P(f"Mauvaise réponse. La réponse était {correct_answer[indexQuestion]}")
     else:
-        return "Mauvaise réponse, la réponse était ", reponse_attendue
-
+        return ""
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8043)
+    app.run_server(debug=True, host="0.0.0.0", port=8066)
