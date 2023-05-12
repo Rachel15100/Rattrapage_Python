b66ee842c3e910f33451a91066d3736457eb1c20
Ubuntu <ubuntu@ip-172-31-41-82.eu-west-3.compute.internal>
2023-05-12 19:37:06 +0000

    Add the 20 questions in the dico

--- my_final_app.py
+++ my_final_app.py
@@ -9,7 +9,26 @@ server=app.server
 dico=dict()
 dico={"Quelle commande permet de créer une nouvelle branche ?":"git branch",
       "Quel est l'opérateur de comparaison qui permet de vérifier si deux valeurs sont égales ?":"==",
-      "Quelle commande linux permet de lister le contenu d'un répertoire ?": "ls"}
+      "Quelle commande linux permet de lister le contenu d'un répertoire ?": "ls", 
+      "Quelle commande permet de changer de répertoire ?": "cd",
+      "Quelle commande permet de supprimer un répertoire et son contenu ?":"rm -rv", 
+      "Quelle commande permet de rechercher une chaine de caractère dans un fichier ?":"grep",
+      "Quelle commande permet de changer de branche ?":"git checkout",
+      "Quelle commande permet de créer une copie locale d'une repository git ?":"git clone",
+      "Quelle commande linux permet de planifier l'exécution de commandes ou de scripts à intervals réguliers  ?":"cron",
+      "Quel package python permet la création d'un tableau de bord ?":"dash",
+      "Quelle commande permet d'afficher le contenu d'un fichier ?":"cat",
+      "Quel outils permet de transférer des données en ligne vers un terminal ?":"curl",
+      'cat fichier.txt | grep -oP "linux\s\w+" : renvoie le mot linux seul. Vrai ou Faux ?':"Faux",
+      "Qu'est ce que Linux ? (2 mots)":"Système d'exploitation",
+      "Quelle est l'extention pour un script shell ? (attention au '.')":".sh",
+      "La commande : */5 * * * * /chemin/vers/le/script.sh, exécute le script.sh toutes les 5 minutes. Vrai ou Faux ?":"Vrai",
+      "Quelle est la commande pour créer un nouveau repository en linux ?":"mkdir",
+      "Quel est le symbole d'un chevron en linux ?":">",
+      "Quelle est la commande pour saisir du texte dans un fichier texte linux ? (4 lettres)":"echo",
+      "Quelle est la commande pour changer les permissions d'un fichier ?":"chmod",
+      "Quelle sont les lettres permettant à un utilisateur de lire, écrire et exécuter un fichier ?":"rwx",
+      "Quelle commande permet d'exécuter une command en tant que super utilisateur ?":"sudo"}
 
 
 def serve_layout():
@@ -23,7 +42,7 @@ def serve_layout():
         html.Div(id='output'),
         html.H5("Veuillez actualiser la page pour une accéder à une autre question"),
         html.Br(),
-        html.A(html.Button('random question', id='refresh-button'), href='http://13.39.83.94:8025')
+        html.A(html.Button('random question', id='refresh-button'), href='http://13.39.83.94:8017')
     ])
     
     return layout
@@ -49,4 +68,4 @@ def verification_Dico(n_clicks, reponse_utilisateur, question_text):
         return ""
 
 if __name__=='__main__':
-    app.run(debug=True, host="0.0.0.0",  port=8025)
+    app.run(debug=True, host="0.0.0.0",  port=8017)
